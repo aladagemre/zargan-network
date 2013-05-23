397e4c0e439c3a90b2189e3eec78c70e6c6b5584
Ahmet Emre Aladağ <aladagemre@gmail.com>
2013-05-23 20:59:03 +0300

    Improvements on pre-filtering

--- zargan/clear.py
+++ zargan/clear.py
@@ -1,13 +1,48 @@
+# encoding: utf-8
 import codecs
 
-f = codecs.open("zargan/data/stats20110912-01.txt", encoding="latin5")
-o = open("zargan/data/filtered.txt", "w")
+def filter_campaign(cols):
+    """
+    Returns True if the search is a campaign search and not valuable.
+    """
+    words = [u"unfamşiar", u"kanıtlamk", u"toplam değişken", u"çalışmak zorunda kalmak", u"conservative", \
+             u"conservation", u"conservativative", u"irregular", u"unfair", u"kani"]
 
-o.write(f.readline())
-for line in f:
-    if line.count("|") != 11:
-        continue
+    if cols[0].lower() in words:
+        return True
 
-    o.write(line.encode("utf8"))
-o.close()
 
+def filter_corporation(cols):
+    """
+    Returns True if the search has corporate id. We won't use corporate search logs.
+    """
+    return cols[5]
+
+def filter_ip(cols):
+    ips = []
+    if cols[1] in ips:
+        return True
+
+def main():
+    f = codecs.open("zargan/data/stats20110912-01.txt", encoding="latin5")
+    o = open("zargan/data/filtered.txt", "w")
+
+    o.write(f.readline())
+    for line in f:
+        cols = line.split("|")
+
+        if len(cols) != 12:
+            continue
+        if filter_ip(cols):
+            continue
+        if filter_corporation(cols):
+            continue
+        if filter_campaign(cols):
+            continue
+
+        o.write(line.encode("utf8"))
+    o.close()
+
+
+if __name__ == "__main__":
+    main()
